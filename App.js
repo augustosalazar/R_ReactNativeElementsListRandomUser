e7efaca387f635f5b78ec95e04100d060c31bca3
augustosalazar <augustosalazar@uninorte.edu.co>
2018-04-30 14:27:23 -0500

    initial

---
+++ App.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { FlatList, Text, View } from "react-native";
+import { List, ListItem } from "react-native-elements";
+
+//https://raw.githubusercontent.com/benawad/flatlists/1_get_data_from_api/src/index.js
+
+export default class App extends Component {
+  state = {
+    data: []
+  };
+
+  componentWillMount() {
+    this.fetchData();
+  }
+
+  fetchData = async () => {
+    const response = await fetch("https://randomuser.me/api?results=10");
+    const json = await response.json();
+    this.setState({ data: json.results });
+  };
+
+  render() {
+    return (
+      <View>
+        <List>
+          <FlatList
+            data={this.state.data}
+            keyExtractor={(x, i) => i}
+            renderItem={({ item }) =>
+              <ListItem
+                roundAvatar
+                avatar={{ uri: item.picture.thumbnail }}
+                title={`${item.name.first} ${item.name.last}`}
+              />}
+          />
+        </List>
+      </View>
+    );
+  }
+}
